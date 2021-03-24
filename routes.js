954311f1a14e552bf9a2ef3b7ad1b131fc5fb2ce
cemarv25 <cesar.martinez0010@outlook.com>
2021-03-23 19:14:03 -0700

    AM-12 Added signUp function to create a user

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
-router = require('express').Router(); 
+router = require('express').Router();
 
 const requestController = require('./controller/requestController');
 
 router.post('/message', requestController.handleMessage);
+
+// User routes
+router.post('/signUp', requestController.signUp);
 
 module.exports = router;
