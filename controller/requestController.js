8bf87dbc49ffc34ae048cfbf62ceafbfc0e20c91
cemarv25 <cesar.martinez0010@outlook.com>
2021-03-23 19:14:03 -0700

    AM-12 Added signUp function to create a user

--- controller/requestController.js
+++ controller/requestController.js
@@ -1,15 +1,72 @@
-const {Wit, log} = require('node-wit');
-async function test(req, res) {
-    const client = new Wit({
-        accessToken: req.body.accessToken,
-        logger: new log.Logger(log.DEBUG)
-    });
+const { Wit, log } = require('node-wit');
+const bcrypt = require('bcrypt');
 
-    const message = req.body.message;
-    const wit_response = client.message(message);
-    res.json(wit_response);
+const User = require('../models/user');
+
+async function test(req, res) {
+  const client = new Wit({
+    accessToken: req.body.accessToken,
+    logger: new log.Logger(log.DEBUG),
+  });
+
+  const message = req.body.message;
+  const wit_response = client.message(message);
+  res.json(wit_response);
+}
+
+async function signUp(req, res) {
+  // Get email and password from request body
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const users = await User.find().catch((err) => {
+    console.log('error while trying to get users');
+    console.log(err);
+  });
+
+  // If user already exists
+  if (users.length >= 1) {
+    return res.status(500).json({
+      message: 'A user registered with that email already exists',
+    });
+    // If user doesn't exist
+  } else {
+    await bcrypt
+      .hash(password, 10)
+      .then((hashedPassword) => {
+        console.log('hash successfully created, creating user');
+        // Create user
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+        });
+
+        user
+          .save()
+          .then((result) => {
+            console.log(result);
+            res.status(201).json({
+              message: 'You have created an account',
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          });
+      })
+      .catch((err) => {
+        console.log('error while hashing password');
+        console.log(err);
+        return res.status(500).json({
+          error: err,
+        });
+      });
+  }
 }
 
 module.exports = {
-    test
+  test,
+  signUp,
 };
--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
-router = require('express').Router(); 
+router = require('express').Router();
 
 const requestController = require('./controller/requestController');
 
 router.post('/message', requestController.test);
+
+// User routes
+router.post('/signUp', requestController.signUp);
 
 module.exports = router;
