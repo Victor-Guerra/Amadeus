6408586858fdbe2e71a220333db7d56339668fc1
cemarv25 <cesar.martinez0010@outlook.com>
2021-03-26 13:11:20 -0700

    Fixed deleted bracket in signUp function

--- controller/requestController.js
+++ controller/requestController.js
@@ -224,6 +224,13 @@ async function signUp(req, res) {
   }
 }
 
+async function handleMessage(req, res) {
+  const message = req.body.message;
+  const wit_response = await client.message(message);
+  const bot_response = await responseManager.getResponse(wit_response);
+  res.json(bot_response);
+}
+
 module.exports = {
   handleTestMessage,
   signUp,
