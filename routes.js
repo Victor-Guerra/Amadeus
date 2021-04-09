41d2b261228d6e74dc77a94349dd9104de0420a1
cemarv25 <cesar.martinez0010@outlook.com>
2021-04-08 20:33:40 -0600

    AM-13 Added login function and route

--- routes.js
+++ routes.js
@@ -9,7 +9,4 @@ router.post('/message', requestController.handleMessage);
 router.post('/signUp', requestController.signUp);
 router.post('/logIn', requestController.logIn);
 
-// User routes
-router.post('/signUp', requestController.signUp);
-
 module.exports = router;
