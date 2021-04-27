5c2d5755815652e30e61e6206ca55a5a8c2c8242
cemarv25 <cesar.martinez0010@outlook.com>
2021-04-27 14:38:28 -0600

    AM-13 Added tests for login

--- test/AR_23.test.js
+++ test/AR_23.test.js
@@ -2,17 +2,19 @@ const http = require('http-request');
 const reqController = require('../controller/requestController');
 
 test('Sends an example message to the wit.ai, then back to the api', () => {
-    http.post({
-            url: "http://localhost:3000/api/message",
-            method: "POST",
-            reqBody: {
-               "message": "How are you?"
-           }
-           }, function (err, res)  {
-            if(err) {
-                console.error(err);
-            }
-            return expect(res).resolves.toEqual('Thanks ^^');
-                    
-    });
+  http.post(
+    {
+      url: 'http://localhost:3000/api/message',
+      method: 'POST',
+      reqBody: {
+        message: 'How are you?',
+      },
+    },
+    function (err, res) {
+      if (err) {
+        console.error(err);
+      }
+      return expect(res).resolves.toEqual('Thanks ^^');
+    }
+  );
 });
